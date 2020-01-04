00aeaea9fdfe6f2e6668f117acaf1d24a6d84edc
根岸 渉 <teteruru3690@gmail.com>
2020-01-04 15:21:37 +0900

    Inputクラスの作成

--- Balance/Input.h
+++ Balance/Input.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <DxLib.h>
 #include <string>
+#include "Vector2.h"
 
 class Input {
 
@@ -15,4 +16,14 @@ public:
 	int GetInputState();
 	int GetInputState(int inputType);
 
+	Vector2 GetAnalog();
+	Vector2 GetAnalog(int inputType);
+
+	int GetAnalogX();
+	int GetAnalogX(int inputType);
+
+	void StartJoypadVibration(int Power, int Time);
+	void StopJoypadVibration();
+	void StartJoypadVibration(int Power, int Time, int inputType);
+	void StopJoypadVibration(int inputType);
 };
