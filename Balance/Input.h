e0efb13f1d1c940e611855dc729496d2ceadb565
根岸 渉 <teteruru3690@gmail.com>
2020-01-06 22:21:30 +0900

    defineでのボタンの対応(Input.hをincludeしてると使えます) INPUT_BUTTON_〇　で使えます

--- Balance/Input.h
+++ Balance/Input.h
@@ -3,6 +3,23 @@
 #include <string>
 #include "Vector2.h"
 
+#define INPUT_BUTTON_A 16
+#define INPUT_BUTTON_B 32
+#define INPUT_BUTTON_X 64
+#define INPUT_BUTTON_Y 128
+#define INPUT_BUTTON_LB 256
+#define INPUT_BUTTON_RB 512
+#define INPUT_BUTTON_START 2048
+#define INPUT_BUTTON_BACK 1024
+#define INPUT_BUTTON_UP 8
+#define INPUT_BUTTON_DOWN 1
+#define INPUT_BUTTON_RIGHT 4
+#define INPUT_BUTTON_LEFT 2
+#define INPUT_BUTTON_LEFT_UP 10
+#define INPUT_BUTTON_LEFT_DOWN 3
+#define INPUT_BUTTON_RIGHT_DOWN 5
+#define INPUT_BUTTON_RIGHT_UP 12
+
 class Input {
 
 private:
